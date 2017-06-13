9c2627238bb530e6e1cc47585099c1cc6919f6b2
alaaGnadrey <alaa.gnadrey@gmail.com>
2017-06-13 09:22:03 +0300

    Add files via upload

---
+++ routes/routesMiddleware.js
@@ -0,0 +1,14 @@
+
+var middleWareFunctions={
+    isLoggedIn:function (req, res, next) {
+       // route middleware to make sure a user is logged in
+      // if user is authenticated in the session, carry on 
+      if (req.isAuthenticated())
+          return next();
+
+      // if they aren't redirect them to the home page
+      res.redirect('/login');
+    }
+};
+
+module.exports=middleWareFunctions;
